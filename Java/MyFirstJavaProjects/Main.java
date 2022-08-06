c2bf71a1c9159b1145015d2caf4d9755af053712
ghorghanlu <ghorghanlu@gmail.com>
2022-08-06 16:11:21 +0430

    My first Java project

---
+++ Java/MyFirstJavaProjects/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+    System.out.println("hello SAJJAD");
+    System.out.print("hello SAJJADiiiiiiiiii");
+    System.out.println("hello SAJJADlllllllll");
+
+    }
+
+}
+
+
