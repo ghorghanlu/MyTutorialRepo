9ac6c8479657267c81b728bcdafc13d786d4a5dc
Sajjad Ghorghanlu <62830901+ghorghanlu@users.noreply.github.com>
2022-08-06 18:46:18 +0430

    Update DiceRollSimulator.py

--- Python/Python mini-projects/DiceRollSimulator.py
+++ Python/Python mini-projects/DiceRollSimulator.py
@@ -1,8 +1,8 @@
 import random
 
 list = [1, 2, 3, 4, 5, 6]
+
 while True:
-    
     userInput = int(input(" 1. roll the dice     2. exit     "))
     if userInput == 1:
         x = random.choice(list) # random.randint(1,6)
