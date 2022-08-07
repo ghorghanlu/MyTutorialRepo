25d81344189ff4fe79f22319ce4cd508e03ff047
ghorghanlu <ghorghanlu@gmail.com>
2022-08-07 16:28:40 +0430

    Dice roll simulator

--- Python/Python Mini Projects/DiceRollSimulator.py
+++ Python/Python Mini Projects/DiceRollSimulator.py
@@ -1,3 +1,13 @@
+# Dice roll simulator
+"""
+The goal is to create a program that will simulate the roll of dice.
+
+Topics: random module, looping, and if-else
+
+Hint: Using a random module generate a random number between 
+the range from 1 to 6 when the user wants.
+"""
+
 import random
 
 list = [1, 2, 3, 4, 5, 6]
