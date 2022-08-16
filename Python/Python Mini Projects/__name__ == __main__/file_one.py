444f7053f0019863d5c022eead319d9fd3a88a2d
ghorghanlu <ghorghanlu@gmail.com>
2022-08-16 18:39:56 +0430

    The functions are loaded but not run

--- Python/Python Mini Projects/__name__ == __main__/file_one.py
+++ Python/Python Mini Projects/__name__ == __main__/file_one.py
@@ -1,7 +1,13 @@
-# Python file one module
+# Python module to execute
 import file_two
 
-print("File one __name__ is set to: {}".format(__name__))
+print("File one __name__ is set to: {}" .format(__name__))
+
+def function_one():
+   print("Function one is executed")
+
+def function_two():
+   print("Function two is executed")
 
 if __name__ == "__main__":
    print("File one executed when ran directly")
